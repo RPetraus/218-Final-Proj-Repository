81f1d6b79b6f3ea821108c53e5137db86645192d
RPetraus <ryan.h.petrauskas@gmail.com>
2025-03-20 00:44:23 -0400

    Update pc_serial_com.cpp

--- modules/pc_serial_com/pc_serial_com.cpp
+++ modules/pc_serial_com/pc_serial_com.cpp
@@ -22,15 +22,31 @@ UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 //=====[Declarations (prototypes) of private functions]========================
 
 //=====[Implementations of public functions]===================================
+
 /**
-* Sends a string over the serial interface.
-* Parameter: The string to be written to the interface
-*/
+ * Sends a string over the serial interface.
+ * 
+ * This function writes a null-terminated string to the serial interface 
+ * using the configured UART communication. It sends the string byte by byte 
+ * over the USB serial port.
+ *
+ * @param str The string to be written to the interface.
+ * @return None
+ */
 void pcSerialComStringWrite( const char* str )
 {
     uartUsb.write( str, strlen(str) );
 }
 
+/**
+ * Reads a single character from the serial interface.
+ * 
+ * This function waits until a character is received over the serial interface 
+ * and then stores it in the provided variable.
+ *
+ * @param receivedChar A pointer to a variable where the received character will be stored.
+ * @return None
+ */
 void pcSerialComCharRead( char *receivedChar ) {
     uartUsb.read( receivedChar, 1 );
 }
